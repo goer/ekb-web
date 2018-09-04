7a1bcf8262182219009bb56d4c71c3d2f8b57fe0
Benediktus Zebua <benedyctoes@gmail.com>
2018-09-04 15:50:36 +0700

    push email

---
+++ contactform/contactform.php
@@ -0,0 +1,22 @@
+<?php
+	if(isset($_POST['submit'])){
+		$name=$_POST['name'];
+        $email=$_POST['email'];
+        $namepic=$_POST['namepic'];
+		$phone=$_POST['phone'];
+		$msg=$_POST['msg'];
+
+		// $to='info@e-kasbon.com'; // Receiver Email ID, Replace with your email ID
+		$to='benedyctoes@gmail.com'; // Receiver Email ID, Replace with your email ID
+		$subject='Form Submission';
+		$message="Name :".$name."\n"."Phone :".$phone."\n"."Wrote the following :"."\n\n".$msg;
+		$headers="From: ".$email;
+
+		if(mail($to, $subject, $message, $headers)){
+			echo "<h1>Sent Successfully! Thank you"." ".$name.", We will contact you shortly!</h1>";
+		}
+		else{
+			echo "Something went wrong!";
+		}
+	}
+?>
