13170db401a3fc3898784f1d534e008d75f6217b
Benediktus Zebua <benedyctoes@gmail.com>
2018-09-04 16:58:45 +0700

    push email 5

--- contactform/contactform.php
+++ contactform/contactform.php
@@ -1,23 +1,26 @@
 <?php
-	if(isset($_POST['submit'])){
-		$name=$_POST['name'];
-        $email=$_POST['email'];
-        $namepic=$_POST['namepic'];
-		$phone=$_POST['phone'];
-		// $msg=$_POST['msg'];
+	// if(isset($_POST['submit'])){
+	// 	$name=$_POST['name'];
+    //     $email=$_POST['email'];
+    //     $namepic=$_POST['namepic'];
+	// 	$phone=$_POST['phone'];
+	// 	// $msg=$_POST['msg'];
 
-		// $to='info@e-kasbon.com'; // Receiver Email ID, Replace with your email ID
-		$to='benedyctoes@gmail.com'; // Receiver Email ID, Replace with your email ID
-		$subject='Form Submission';
-		$message="Name :".$name."\n"."Phone :".$phone."\n"."Name PIC :"."\n\n".$namepic;
-		$headers="From: ".$email;
-
-		if(mail($to, $subject, $message, $headers)){
-			// echo "<h1>Sent Successfully! Thank you"." ".$name.", We will contact you shortly!</h1>";
-			echo "<h1>Sent Successfully! Thank you</h1>";
-		}
-		else{
-			echo "Something went wrong!";
-		}
+	// 	// $to='info@e-kasbon.com'; // Receiver Email ID, Replace with your email ID
+	// 	$to='benedyctoes@gmail.com'; // Receiver Email ID, Replace with your email ID
+	// 	$subject='Form Submission';
+	// 	$message="Name :".$name."\n"."Phone :".$phone."\n"."Name PIC :"."\n\n".$namepic;
+	// 	$headers="From: ".$email;
+	// }
+	
+	if($_POST){
+		$name = $_POST['name'];
+		$email = $_POST['email'];
+		$namepic = $_POST['namepic'];
+		$phone = $_POST['phone'];
+	
+	//send email
+		mail("benedyctoes@gmail.com", "This is an email from:" .$name, $email);
 	}
+    
 ?>
